2df8a45d0e63c81a17400859a46b0338d64df184
rspousta <spousta.radek@seznam.cz>
2015-07-21 19:09:31 +0200

    GPU overclocc not working -> remove it

--- arch/arm/mach-msm/board-8930-gpu.c
+++ arch/arm/mach-msm/board-8930-gpu.c
@@ -116,11 +116,6 @@ static struct kgsl_device_iommu_data kgsl_3d0_iommu_data[] = {
 static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	.pwrlevel = {
 		{
-			.gpu_freq = 500000000,
-			.bus_freq = 4,
-			.io_fraction = 0,
-		},
-		{
 			.gpu_freq = 400000000,
 			.bus_freq = 3,
 			.io_fraction = 0,
@@ -141,7 +136,7 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 		},
 	},
 	.init_level = 1,
-	.num_levels = 5,
+	.num_levels = 4,
 	.set_grp_async = NULL,
 	.idle_timeout = HZ/12,
 	.nap_allowed = true,
@@ -172,7 +167,7 @@ void __init msm8930_init_gpu(void)
 
 	if (cpu_is_msm8930aa())
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 450000000;
-	else if (cpu_is_msm8930ab() || true) {
+	else if (cpu_is_msm8930ab() ) {
 		kgsl_3d0_pdata.pwrlevel[0].gpu_freq = 500000000;
 		grp3d_max_vectors[0].ib = KGSL_CONVERT_TO_MBPS(4800);
 	}
